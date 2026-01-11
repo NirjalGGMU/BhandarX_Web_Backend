e4a138900a36196400abcc4858f868feaf1a4df4
Nirjal Adhikari <anirjal08@gmail.com>
2026-01-11 14:43:11 +0545

    index commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+// import express from "express";
+// import { config } from "./config";
+// import { connectDB } from "./database/mongodb";
+// import authRoutes from "./routes/auth.route";
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+
+// connectDB();
+
+// app.listen(config.port, () => {
+//   console.log(`🚀 Server running on port ${config.port}`);
+// });
+
+
+
 import express from "express";
 import { config } from "./config";
 import { connectDB } from "./database/mongodb";
