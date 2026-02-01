aefc2158f37cc907fda7a5b57697d55f1c5a4fa3
Nirjal Adhikari <anirjal08@gmail.com>
2026-02-01 21:59:34 +0545

    Update user.service.ts in services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,53 +1,55 @@
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { config } from "../config";
-import { createUser, findUserByEmail } from "../repositories/user.repository";
+import { getUserByEmail, createUser } from "../repositories/user.repository";
 import { HttpError } from "../errors/http-error";
-import { LoginUserInput, RegisterUserInput } from "../types/user.type";
+import { JWT_SECRET } from "../config"; // Ensure secret is imported
 
-export const registerUserService = async (input: RegisterUserInput) => {
-  const existingUser = await findUserByEmail(input.email);
-
+export const registerUser = async (data: any) => {
+  const existingUser = await getUserByEmail(data.email);
   if (existingUser) {
-    throw new HttpError("Email already exists", 409);
+    throw new HttpError(400, "Email already exists");
   }
 
-  const hashedPassword = await bcrypt.hash(input.password, 10);
+  const hashedPassword = await bcryptjs.hash(data.password, 10);
 
-  const user = await createUser({
-    name: input.name,
-    email: input.email,
+  const newUser = await createUser({
+    name: data.name,
+    email: data.email,
     password: hashedPassword,
     role: "user",
   });
 
   return {
-    id: user._id,
-    email: user.email,
-    role: user.role,
+    message: "User created successfully",
+    user: {
+      id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    },
   };
 };
 
-export const loginUserService = async (input: LoginUserInput) => {
-  const user = await findUserByEmail(input.email);
-
+export const loginUser = async (data: any) => {
+  const user = await getUserByEmail(data.email);
   if (!user) {
-    throw new HttpError("Invalid email or password", 401);
+    throw new HttpError(401, "Invalid email or password");
   }
 
-  const isPasswordValid = await bcrypt.compare(input.password, user.password);
-
-  if (!isPasswordValid) {
-    throw new HttpError("Invalid email or password", 401);
+  const isValid = await bcryptjs.compare(data.password, user.password);
+  if (!isValid) {
+    throw new HttpError(401, "Invalid email or password");
   }
 
   const token = jwt.sign(
-    { userId: user._id, role: user.role },
-    config.jwtSecret,
-    { expiresIn: "1d" }
+    { id: user._id, email: user.email, role: user.role },
+    JWT_SECRET,
+    { expiresIn: "7d" } // Set expiration
   );
 
   return {
+    success: true,
+    message: "Login successful",
     token,
     user: {
       id: user._id,
@@ -57,3 +59,52 @@ export const loginUserService = async (input: LoginUserInput) => {
     },
   };
 };
+
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
+// import { config } from "../config";
+// import { createUser, findUserByEmail } from "../repositories/user.repository";
+// import { HttpError } from "../errors/http-error";
+// import { LoginUserInput, RegisterUserInput } from "../types/user.type";
+
+// export const registerUserService = async (input: RegisterUserInput) => {
+//   const existingUser = await findUserByEmail(input.email);
+
+//   if (existingUser) throw new HttpError("Email already exists", 409);
+
+//   const hashedPassword = await bcrypt.hash(input.password, 10);
+
+//   const user = await createUser({
+//     name: input.name,
+//     email: input.email,
+//     password: hashedPassword,
+//     role: input.role, // now uses input.role
+//   });
+
+//   return {
+//     id: user._id,
+//     email: user.email,
+//     role: user.role,
+//     name: user.name,
+//   };
+// };
+
+// export const loginUserService = async (input: LoginUserInput) => {
+//   const user = await findUserByEmail(input.email);
+//   if (!user) throw new HttpError("Invalid email or password", 401);
+
+//   const isPasswordValid = await bcrypt.compare(input.password, user.password);
+//   if (!isPasswordValid) throw new HttpError("Invalid email or password", 401);
+
+//   const token = jwt.sign({ userId: user._id, role: user.role }, config.jwtSecret, { expiresIn: "1d" });
+
+//   return {
+//     token,
+//     user: {
+//       id: user._id,
+//       name: user.name,
+//       email: user.email,
+//       role: user.role,
+//     },
+//   };
+// };
