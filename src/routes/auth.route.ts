4d9f385ddd705a0428945892f096c8bf61664bbe
Nirjal Adhikari <anirjal08@gmail.com>
2026-02-01 21:59:19 +0545

    Update auth.route.ts in routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controller";
+import { register, login } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
+
+// import { Router } from "express";
+// import { loginUser, registerUser } from "../controllers/auth.controller";
+
+// const router = Router();
+
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
+
+// export default router;
