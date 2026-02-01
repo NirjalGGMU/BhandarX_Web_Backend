45133bf6fcabf0129340edd21dab4010594eeaad
Nirjal Adhikari <anirjal08@gmail.com>
2026-02-01 21:59:29 +0545

    Update user.repository.ts in repositories

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,9 +1,20 @@
-import { User, IUser } from "../models/user.model";
+import { UserModel } from "../models/user.model";
 
-export const findUserByEmail = async (email: string): Promise<IUser | null> => {
-  return User.findOne({ email });
+export const getUserByEmail = async (email: string) => {
+  return await UserModel.findOne({ email });
 };
 
-export const createUser = async (data: Partial<IUser>): Promise<IUser> => {
-  return User.create(data);
+export const createUser = async (userData: any) => {
+  const user = new UserModel(userData);
+  return await user.save();
 };
+
+// import { User, IUser } from "../models/user.model";
+
+// export const findUserByEmail = async (email: string): Promise<IUser | null> => {
+//   return User.findOne({ email });
+// };
+
+// export const createUser = async (data: Partial<IUser>): Promise<IUser> => {
+//   return User.create(data);
+// };
