f61b4cbafbe1e666bd957851dc3857f41b14bdef
Nirjal Adhikari <anirjal08@gmail.com>
2026-02-03 19:17:24 +0545

    Updated src/errors/http-error.ts

--- src/errors/http-error.ts
+++ src/errors/http-error.ts
@@ -1,11 +1,19 @@
 // first is only coorect from line no 3-10
 
 export class HttpError extends Error {
-    statusCode: number;
-  
-    constructor(message: string, statusCode: number) {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
       super(message);
       this.statusCode = statusCode;
-    }
+      this.name = 'HttpError';
+      
+      // Maintains proper stack trace for where our error was thrown (only available on V8)
+      if (Error.captureStackTrace) {
+          Error.captureStackTrace(this, HttpError);
+      }
+      
+      // Fix for prototype chain
+      Object.setPrototypeOf(this, new.target.prototype);
   }
-  
+}
