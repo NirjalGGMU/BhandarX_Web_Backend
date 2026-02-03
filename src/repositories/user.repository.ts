2f714cca8189eebb1ad59a415ab505dd3055e727
Nirjal Adhikari <anirjal08@gmail.com>
2026-02-03 19:17:24 +0545

    Updated src/repositories/user.repository.ts

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,29 +1,68 @@
 import { UserModel } from "../models/user.model";
+import { CreateUserDTO, UpdateUserDTO } from "../dtos/user.dto";
 
-export const getUserByEmail = async (email: string) => {
-  return await UserModel.findOne({ email });
-};
+export class UserRepository {
+    async getUserByEmail(email: string) {
+        return await UserModel.findOne({ email });
+    }
 
-export const createUser = async (userData: any) => {
-  const user = new UserModel(userData);
-  return await user.save();
-};
+    async getUserByUsername(username: string) {
+        return await UserModel.findOne({ username });
+    }
 
-export const findAllUsers = async () => {
-  return await UserModel.find().select("-password");
-};
+    async createUser(userData: CreateUserDTO) {
+        const user = new UserModel(userData);
+        return await user.save();
+    }
 
-export const findUserById = async (id: string) => {
-  return await UserModel.findById(id).select("-password");
-};
+    async getUserById(id: string) {
+        return await UserModel.findById(id).select('-password');
+    }
 
-export const updateUserById = async (id: string, updateData: any) => {
-  return await UserModel.findByIdAndUpdate(id, updateData, { new: true }).select("-password");
-};
+    async updateUser(id: string, updateData: UpdateUserDTO) {
+        return await UserModel.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true }
+        ).select('-password');
+    }
 
-export const deleteUserById = async (id: string) => {
-  return await UserModel.findByIdAndDelete(id);
-};
+    async deleteUser(id: string) {
+        return await UserModel.findByIdAndDelete(id);
+    }
+
+    async getAllUsers() {
+        return await UserModel.find().select('-password');
+    }
+}
+
+
+// import { UserModel } from "../models/user.model";
+
+// export const getUserByEmail = async (email: string) => {
+//   return await UserModel.findOne({ email });
+// };
+
+// export const createUser = async (userData: any) => {
+//   const user = new UserModel(userData);
+//   return await user.save();
+// };
+
+// export const findAllUsers = async () => {
+//   return await UserModel.find().select("-password");
+// };
+
+// export const findUserById = async (id: string) => {
+//   return await UserModel.findById(id).select("-password");
+// };
+
+// export const updateUserById = async (id: string, updateData: any) => {
+//   return await UserModel.findByIdAndUpdate(id, updateData, { new: true }).select("-password");
+// };
+
+// export const deleteUserById = async (id: string) => {
+//   return await UserModel.findByIdAndDelete(id);
+// };
 
 // import { UserModel } from "../models/user.model";
 
